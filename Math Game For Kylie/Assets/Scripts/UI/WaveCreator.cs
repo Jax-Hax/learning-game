7c40077e9d44bdd1f07dab40647c5e770ad72413
Jax-Hax <94689952+Jax-Hax@users.noreply.github.com>
2022-05-18 07:32:39 -0400

    Update WaveCreator.cs

--- Math Game For Kylie/Assets/Scripts/UI/WaveCreator.cs
+++ Math Game For Kylie/Assets/Scripts/UI/WaveCreator.cs
@@ -1,1 +1,9 @@
-
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using TMPro;
+public class WaveCreator : MonoBehaviour
+{
+    public GameObject NoSetEnabled;
+}
