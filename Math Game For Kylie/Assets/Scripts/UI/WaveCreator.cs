ce26eb7d2551eae942c92c57e6a1ce1276fb21ff
Jax-Hax <94689952+Jax-Hax@users.noreply.github.com>
2022-06-14 11:52:38 -0400

    roundbuilder p2

--- Math Game For Kylie/Assets/Scripts/UI/WaveCreator.cs
+++ Math Game For Kylie/Assets/Scripts/UI/WaveCreator.cs
@@ -15,8 +15,14 @@ public class WaveCreator : MonoBehaviour
     public GameObject LobbyObject;
     private GameObject LobbyObjectPri;
     private ModeCard modeCard;
+    public GameObject RoundObject;
+    private GameObject RoundObjectPri;
+    private RoundCard roundCard;
     private int setInfoIndex = 0;
+    private int setInfoIndex2 = 0;
+    private string[] setInfo2;
     public Transform listPosForSet;
+    public Transform listPosForRound;
     private WaveCreator waveCreate;
     private void Start()
     {
@@ -28,31 +34,46 @@ public class WaveCreator : MonoBehaviour
         SetViewer.SetActive(false);
         Modebuilder.SetActive(true);
         setInfo = loadedString.Split(char.Parse("{"));
+        setInfoIndex = 0;
+        foreach (string currentInfo in setInfo)
+        {
+            RoundObjectPri = Instantiate(RoundObject, listPosForRound);
+            roundCard = RoundObjectPri.GetComponent<RoundCard>();
+            if (setInfoIndex == 0)
+            {
+                setInfoIndex++;
+            }
+            else if (setInfoIndex == 1)
+            {
+                setInfoIndex++;
+            }
+            else
+            {
+                setInfoIndex2 = 0;
+                setInfo2 = loadedString.Split(char.Parse(":"));
+                foreach (string currentInfo2 in setInfo2)
+                {
+                    if (setInfoIndex2 == 0)
+                    {
+                        setInfoIndex2++;
+                    }
+                    else if (setInfoIndex2 == 1)
+                    {
+                        setInfoIndex2 = 0;
+                        break;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
     }
     public void CreateNewSet()
     {
         SetViewer.SetActive(false);
         Modebuilder.SetActive(true);
-        setInfoIndex = 0;
-        foreach (string currentInfo in setInfo)
-        {
-            if (setInfoIndex == 0)
-            {
-                modeCard.title = currentInfo;
-                modeCard.pathToDelete = Application.persistentDataPath + "/modesave" + currentInfo + ".scree";
-                modeCard.loadableString = loadableString;
-                modeCard.listPosForSet = listPosForSet;
-                modeCard.waveCreator = waveCreate;
-                setInfoIndex++;
-            }
-            else if (setInfoIndex == 1)
-            {
-                modeCard.amOfRounds = currentInfo;
-                modeCard.UpdateCard();
-                setInfoIndex = 0;
-                break;
-            }
-        }
     }
     public void LoadSetLobby()
     {
