4a4320991efa2556d2263043ec61e039cea9105d
Jax-Hax <94689952+Jax-Hax@users.noreply.github.com>
2022-09-20 17:00:02 -0400

    working on multiplayer cus i cant focux on one thing

--- Math Game For Kylie/Assets/Scripts/Multiplayer/ConnectToServer.cs
+++ Math Game For Kylie/Assets/Scripts/Multiplayer/ConnectToServer.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
 
-public class ConnectToServer : MonoBehaviour
+public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
-
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);)
     }
 }
